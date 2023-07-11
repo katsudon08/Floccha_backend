ee0406db655a3f1028bac6c22d1d75154faafd50
matsu <matsuaki171101@gmail.com>
2023-07-11 10:43:29 +0900

    jsonを返すwebサーバー

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Ping struct {
+	Status int    `json:"status"`
+	Result string `json:"result"`
+}
+
 func main() {
-	
+	analysis := func(w http.ResponseWriter, _ *http.Request) {
+		ping := Ping{http.StatusOK, "Ok"}
+		res, err := json.Marshal(ping)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	}
+
+	http.HandleFunc("/", analysis)
+
+	fmt.Println("starting server: http://localhost:8000")
+	//* ListenAndServeは起動に失敗するとerrorオブジェクトを返すため、その内容を出力する必要があるのでlog.Fatal1で囲んでいる
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
