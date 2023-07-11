41474821416a2d061981b4069f16ab453b1ff270
matsu <matsuaki171101@gmail.com>
2023-07-11 14:49:50 +0900

    モックアップ完成

--- main.go
+++ main.go
@@ -1,38 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-type Ping struct {
-	Status int    `json:"status"`
-	Result string `json:"result"`
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// これを追加しないと、フロント側でエラーを吐いてしまうので注意
+	w.Header().Set("Access-Control-Allow-Origin", "*");
+	hello := []byte("Hello World")
+	_, err := w.Write(hello)
+	if err != nil {
+		log.Fatal(nil)
+	}
 }
 
-//! もう少しモック関数に修正
 func main() {
-	analysis := func(w http.ResponseWriter, r *http.Request) {
-		ping := Ping{http.StatusOK, "Ok"}
-		res, err := json.Marshal(ping)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if r.Method == "POST" {
-			fmt.Println(r)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(res)
-	}
-
-	http.HandleFunc("/", analysis)
-
-	fmt.Println("starting server: http://localhost:8000")
-	//* ListenAndServeは起動に失敗するとerrorオブジェクトを返すため、その内容を出力する必要があるのでlog.Fatal1で囲んでいる
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("http://localhost:8000")
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
