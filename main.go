68ae9d424a537ef23104236373a122239ee0e835
matsu <matsuaki171101@gmail.com>
2023-07-12 09:06:47 +0900

    値の取得に成功

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Ping struct {
+	Status int    `json:"status"`
+	Result string `json:"result"`
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// これを追加しないと、フロント側でエラーを吐いてしまうので注意
-	w.Header().Set("Access-Control-Allow-Origin", "*");
-	hello := []byte("Hello World")
-	_, err := w.Write(hello)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	ping := Ping{
+		Status: http.StatusOK,
+		Result: "ok",
+	}
+	res, _ := json.Marshal(ping)
+
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+
+	fmt.Println("reauest", string(body))
+	_, err := w.Write(res)
 	if err != nil {
 		log.Fatal(nil)
 	}
