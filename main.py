e017377921b003c3f9b5feb8067e83f062ea300c
matsu <matsuaki171101@gmail.com>
2023-08-07 14:10:22 +0900

    次はAST生成部分を作成

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy.symbols import SYM
 import keyword
 import re
 
@@ -6,22 +7,30 @@ def tokeninze(text):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
 
+    tokens = []
+
     firstTokens = ""
     print("BEFORE-----")
     for token in doc:
         print(token.text, token.pos_)
         reTokenText = re.sub("\(", lambda x: f" {x.group()} ", token.text)
         reTokenText = re.sub("\)", lambda x: f"{x.group()} ", reTokenText)
+        reTokenText = re.sub("\%|\/", lambda x: f" {x.group()} ", reTokenText)
         firstTokens += f" {reTokenText}"
         print(firstTokens)
 
     secondTokens = nlp(firstTokens)
     print("AFTER------")
     for token in secondTokens:
+        if token.text == '%':
+            token.pos = SYM
+        tokens.append(token)
         print(token.text, token.pos_)
 
+    for i in tokens:
+        print(i)
+
 tokeninze("""
-for i in range(5):
-    print(i)
+print(1%5)
 """)
 
