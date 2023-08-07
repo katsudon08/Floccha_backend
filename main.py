117767f1f18db2b205c5e8cfef74b04a33feb922
matsu <matsuaki171101@gmail.com>
2023-08-07 12:15:54 +0900

    トークナイズ機構完成

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ def tokeninze(text):
     doc = nlp(text)
 
     firstTokens = ""
+    print("BEFORE-----")
     for token in doc:
+        print(token.text, token.pos_)
         reTokenText = re.sub("\(", lambda x: f" {x.group()} ", token.text)
         reTokenText = re.sub("\)", lambda x: f"{x.group()} ", reTokenText)
         firstTokens += f" {reTokenText}"
         print(firstTokens)
+
+    secondTokens = nlp(firstTokens)
+    print("AFTER------")
+    for token in secondTokens:
+        print(token.text, token.pos_)
 
 tokeninze("""
 for i in range(5):
